9d8bfbcedb59530bea18f9de8e20f6da4bb47306
Alessandro Granato <alessandrogranato1@gmail.com>
2025-10-06 16:05:18 +0200

    Replace RTF with proper Python file

---
+++ dashboard.py
@@ -0,0 +1,25 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 organic_dashboard/\
+\uc0\u9500 \u9472 \u9472  dashboard.py\
+\uc0\u9500 \u9472 \u9472  cart.py\
+\uc0\u9500 \u9472 \u9472  requirements.txt\
+\uc0\u9492 \u9472 \u9472  scrapers/\
+    \uc0\u9492 \u9472 \u9472  nemlig_scraper.py\
+\
+\
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs26 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+\
+\
+\pard\pardeftab720\partightenfactor0
+\cf0 \
+\
+}
